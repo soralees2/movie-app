d051ba93e22df6babad51831d52ef0181482d09f
eelsoras2 <eelsoras2@gmail.com>
2022-01-05 23:22:38 +0900

    create movie app & used fetch method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect,useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+	useEffect(() => {
+		fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+		.then((response) => response.json())
+		.then((json) => {
+			setMovies(json.data.movies)
+		});
+	},[]);
+	console.log(movies);
+	return (
+		<div>{loading ? <h1>Loading...</h1> : null}</div>
+	);
 }
 
 export default App;
