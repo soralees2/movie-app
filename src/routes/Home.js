57e6be0d03c838f5dc00c724933d70d21994cf47
eelsoras2 <eelsoras2@gmail.com>
2022-01-10 23:29:57 +0900

    modi

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home(){
 	const [loading, setLoading] = useState(true);
@@ -31,7 +32,7 @@ function Home(){
 			{loading ? (
 				<h1>Loading...</h1>
 			) : (
-				<div>
+				<div className={styles.movies}>
 					{movies.map((movie) => (
 						<Movie
 							id={movie.id}
