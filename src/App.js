866dbce04e3334e9e31373f28bb675c459dd819e
eelsoras2 <eelsoras2@gmail.com>
2022-01-10 23:22:01 +0900

    url modi

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
 		<Router>
 			{/* Switch는 Route를 찾고 찾으면 컴포넌트를 렌더링함. */}
 			<Routes>
-				<Route path="process.env.PUBLIC_URL/" element={<Home />} />
-				<Route path="process.env.PUBLIC_URL/movie/:id" element={<Detail/>} />
+				<Route path={`${process.env.PUBLIC_URL}/`}  element={<Home />} />
+				<Route  path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail/>} />
 			</Routes>
 		</Router>
 	);
